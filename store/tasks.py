90759341590fb3bd2996abc185df3ae15cc55855
Arsen <arsen.khomiak@gmail.com>
2026-03-12 17:07:30 +0200

    Feature: implement premium email system with hero templates and fix SMTP/SSL

--- store/tasks.py
+++ store/tasks.py
@@ -1,19 +1,72 @@
 from celery import shared_task
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.contrib.auth.models import User
 from .models import Order
 
 @shared_task
 def send_order_confirmation_email(order_id):
     """
-    Task to send an e-mail notification when an order is successfully created.
+    Task to send an HTML e-mail notification when an order is successfully created.
     """
-    order = Order.objects.get(id=order_id)
-    subject = f'Замовлення №{order.id}'
-    message = f'Шановний/а {order.first_name},\n\n' \
-              f'Ви успішно оформили замовлення в нашому магазині.\n' \
-              f'Номер вашого замовлення: {order.id}.'
-    mail_sent = send_mail(subject,
-                          message,
-                          'admin@shop.com',
-                          [order.email])
-    return mail_sent
+    try:
+        order = Order.objects.get(id=order_id)
+        subject = f'Замовлення №{order.id} - Підтвердження'
+        from_email = 'shop@example.com'
+        to = order.email
+
+        html_content = render_to_string('emails/order_confirmation.html', {'order': order})
+        text_content = strip_tags(html_content)
+
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        return True
+    except Order.DoesNotExist:
+        return False
+
+@shared_task
+def send_welcome_email(user_id):
+    """
+    Task to send a welcome email to a newly registered user.
+    """
+    try:
+        user = User.objects.get(id=user_id)
+        subject = f'Ласкаво просимо до нашого магазину, {user.username}!'
+        from_email = 'shop@example.com'
+        to = user.email
+
+        if not to:
+            return False
+
+        html_content = render_to_string('emails/welcome.html', {'user': user})
+        text_content = strip_tags(html_content)
+
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        return True
+    except User.DoesNotExist:
+        return False
+
+@shared_task
+def send_order_status_update_email(order_id):
+    """
+    Task to send an email when an order status changes.
+    """
+    try:
+        order = Order.objects.get(id=order_id)
+        subject = f'Оновлення статусу замовлення №{order.id}'
+        from_email = 'shop@example.com'
+        to = order.email
+
+        html_content = render_to_string('emails/order_status_update.html', {'order': order})
+        text_content = strip_tags(html_content)
+
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        return True
+    except Order.DoesNotExist:
+        return False
